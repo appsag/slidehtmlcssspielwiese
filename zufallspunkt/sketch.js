dab5c18de642a3b7da55830ec1ce3c719966d510
tzeepee <cpannacci@techfak.uni-bielefeld.de>
2023-07-03 14:46:07 +0200

    punkt jetzt

--- zufallspunkt/sketch.js
+++ zufallspunkt/sketch.js
@@ -1,10 +1,10 @@
 function setup() {
   createCanvas(800, 800);
-  frameRate(20);
+  frameRate(1);
   }
   
-  
 function draw() {
+  background(225);
   ellipse(400,400,400);
   line (400,0,400,800);
   line(0,400,800,400);
@@ -12,6 +12,6 @@ function draw() {
   let winkel = (Math.random()*2*Math.PI);
   let x = 400 + 200 * Math.cos(winkel);
   let y = 400 + 200 * Math.sin(winkel);
-  stroke(Math.floor(Math.random()*255),Math.floor(Math.random()*255),Math.floor(Math.random()*255));
   ellipse(x,y,10,10);
+  text("winkel: " + winkel, 100, 100);
   }
